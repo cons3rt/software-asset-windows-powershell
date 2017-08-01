5370d684ca8e8e5ad36142686855e1e308ef69f3
Joe Yennaco <joe@joeyennaco.com>
2017-08-01 17:56:00 -0400

    Updated the sample powershell asset with additional helper functions for setting ASSET_DIR, loading deployment properties as variables, and improved logging capability

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -20,9 +20,9 @@ $env:PATH = [Environment]::GetEnvironmentVariable("PATH", "Machine")
 ########################### VARIABLES ###############################
 
 # Get the CONS3RT environment variables
-$ASSET_DIR = "$env:ASSET_DIR"
-$DEPLOYMENT_HOME = "$env:DEPLOYMENT_HOME"
-$CONS3RT_ROLE_NAME = "$env:CONS3RT_ROLE_NAME"
+$global:ASSET_DIR = $null
+$global:DEPLOYMENT_HOME = $null
+$global:DEPLOYMENT_PROPERTIES = $null
 
 # exit code
 $exitCode = 0
@@ -40,23 +40,98 @@ $LOGFILE = "C:\log\cons3rt-install-$LOGTAG-$TIMESTAMP.log"
 function logger($level, $logstring) {
    $stamp = get-date -f yyyyMMdd-HHmmss
    $logmsg = "$stamp - $LOGTAG - [$level] - $logstring"
-   add-content $Logfile -value $logmsg
+   write-output $logmsg
 }
 function logErr($logstring) { logger "ERROR" $logstring }
 function logWarn($logstring) { logger "WARNING" $logstring }
 function logInfo($logstring) { logger "INFO" $logstring }
 
+function get_asset_dir() {
+    if ($env:ASSET_DIR) {
+        $global:ASSET_DIR = $env:ASSET_DIR
+        return
+    }
+    else {
+        logWarn "ASSET_DIR environment variable not set, attempting to determine..."
+        if (!$PSScriptRoot) {
+            logInfo "Determining script directory using the pre-Powershell v3 method..."
+            $scriptDir = split-path -parent $MyInvocation.MyCommand.Definition
+        }
+        else {
+            logInfo "Determining the script directory using the PSScriptRoot variable..."
+            $scriptDir = $PSScriptRoot
+        }
+        if (!$scriptDir) {
+            $msg =  "Unable to determine the script directory to get ASSET_DIR"
+            logErr $msg
+            throw $msg
+        }
+        else {
+            $global:ASSET_DIR = "$scriptDir\.."
+            logInfo "Determined ASSET_DIR to be: $Aglobal:SSET_DIR"
+        }
+    }
+}
+
+function get_deployment_home() {
+    # Ensure DEPLOYMENT_HOME is set
+    if ($env:DEPLOYMENT_HOME) {
+        $global:DEPLOYMENT_HOME = $env:DEPLOYMENT_HOME
+        logInfo "Found DEPLOYMENT_HOME set to $global:DEPLOYMENT_HOME"
+    }
+    else {
+        logWarn "DEPLOYMENT_HOME is not set, attempting to determine..."
+        # CONS3RT Agent Run directory location
+        $cons3rtAgentRunDir = "C:\cons3rt-agent\run"
+        $deploymentDirName = get-childitem $cons3rtAgentRunDir -name -dir | select-string "Deployment"
+        $deploymentDir = "$cons3rtAgentRunDir\$deploymentDirName"
+        if (test-path $deploymentDir) {
+            $global:DEPLOYMENT_HOME = $deploymentDir
+        }
+        else {
+            $msg = "Unable to determine DEPLOYMENT_HOME from: $deploymentDir"
+            logErr $msg
+            throw $msg
+        }
+    }
+    logInfo "Using DEPLOYMENT_HOME: $global:DEPLOYMENT_HOME"
+}
+
+function get_deployment_properties() {
+    $deploymentPropertiesFile = "$global:DEPLOYMENT_HOME\deployment-properties.ps1"
+    if ( !(test-path $deploymentPropertiesFile) ) {
+        $msg = "Deployment properties not found: $deploymentPropertiesFile"
+        logErr $msg
+        throw $msg
+    }
+    else {
+        $global:DEPLOYMENT_PROPERTIES = $deploymentPropertiesFile
+        logInfo "Found deployment properties file: $global:DEPLOYMENT_PROPERTIES"
+    }
+    import-module $global:DEPLOYMENT_PROPERTIES -force -global
+}
+
 ###################### END HELPER FUNCTIONS ##########################
 
 ######################## SCRIPT EXECUTION ############################
 
-new-item $LOGFILE -itemType file -force
-
+new-item $logfile -itemType file -force
+start-transcript -append -path $logfile
 logInfo "Running $LOGTAG..."
 
 try {
-	logInfo "Installing at: $TIMESTAMP"
-	logInfo "ASSET_DIR: $ASSET_DIR"
+    logInfo "Installing at: $TIMESTAMP"
+    
+    # Set asset dir
+    logInfo "Setting ASSET_DIR..."
+    get_asset_dir
+
+    # Load the deployment properties as variables
+    logInfo "Loading deployment properties..."
+    get_deployment_home
+    get_deployment_properties
+
+	logInfo "ASSET_DIR: $global:ASSET_DIR"
 	$mediaDir="$ASSET_DIR\media"
 
 	# Exit if the media directory is not found
@@ -76,43 +151,6 @@ try {
 	# Get an environment variable
 	$PATH=[Environment]::GetEnvironmentVariable("PATH", "Machine")
 	logInfo "PATH: $PATH"
-
-    # Ensure DEPLOYMENT_HOME is set
-    if ( !$env:DEPLOYMENT_HOME ) {
-        logInfo "DEPLOYMENT_HOME is not set, attempting to determine..."
-        # CONS3RT Agent Run directory location
-        $cons3rtAgentRunDir = "C:\cons3rt-agent\run"
-        $deploymentDirName = get-childitem $cons3rtAgentRunDir -name -dir | select-string "Deployment"
-        $deploymentDir = "$cons3rtAgentRunDir\$deploymentDirName"
-        if (test-path $deploymentDir) {
-            $deploymentHome = $deploymentDir
-        }
-        else {
-            $errMsg = "Unable to determine DEPLOYMENT_HOME from: $deploymentDir"
-            logErr $errMsg
-            throw $errMsg
-        }
-    }
-    else {
-        logInfo "Found DEPLOYMENT_HOME set to $env:DEPLOYMENT_HOME"
-        $deploymentHome = $env:DEPLOYMENT_HOME
-    }
-    logInfo "Using DEPLOYMENT_HOME: $deploymentHome"
-
-	# Load Deployment properties
-    $deploymentPropertiesFile = "$deploymentHome\deployment-properties.ps1"
-    if ( !(test-path $deploymentPropertiesFile) ) {
-        $errMsg = "Deployment properties not found: $deploymentPropertiesFile"
-        logErr $errMsg
-        throw $errMsg
-    }
-    else {
-        logInfo "Found deployment properties file: $deploymentPropertiesFile"
-    }
-
-    # Load deployment properties as a variable, in this case cons3rt_user
-    logInfo "Loading deployment properties..."
-    import-module $deploymentPropertiesFile -force -global
 
     # Ensure your variable was loaded
     if ( !$cons3rt_user ) {
@@ -135,4 +173,7 @@ finally {
 ###################### END SCRIPT EXECUTION ##########################
 
 logInfo "Exiting with code: $exitCode"
+stop-transcript
+get-content -Path $logfile
 exit $exitCode
+
